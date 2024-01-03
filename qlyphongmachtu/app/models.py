b57245f0d0b19e9c4e29370a69e972348f95e237
Phạm Ngọc Trường Sơn <108865028+sonpnts@users.noreply.github.com>
2024-01-03 21:38:39 +0700

    Update models.py

--- qlyphongmachtu/app/models.py
+++ qlyphongmachtu/app/models.py
@@ -19,6 +19,9 @@ class Account(db.Model, UserMixin):
         'polymorphic_on': type
     }
 
+    def __str__(self):
+        return self.id
+
 class Doctor(Account):
     id = Column(Integer, ForeignKey(Account.id), primary_key=True)
     ngayVaoLam = Column(String(50), nullable=False)
@@ -39,9 +42,10 @@ class Patient(Account):
     id = Column(Integer, ForeignKey(Account.id), primary_key=True)
     diaChi = Column(String(50))
     namSinh = Column(String(50))
-    sdt = Column(String(50))
+    sdt = Column(String(50), nullable=False)
     joined_date = Column(DateTime, default=datetime.now())
     #active = Column(Boolean, default=False)
+    books = relationship('Books', cascade="all,delete", backref='patient', lazy=True)
 
     __mapper_args__ = {
         'polymorphic_identity': 'patient'
@@ -50,6 +54,7 @@ class Patient(Account):
 class Nurse(Account):
     id = Column(Integer, ForeignKey(Account.id), primary_key=True)
     phuTrachKhoa = Column(String(50))
+
     __mapper_args__ = {
         'polymorphic_identity': 'nurse'
     }
@@ -62,24 +67,36 @@ class Cashier(Account):
         'polymorphic_identity': 'cashier'
     }
 
+class Time(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    khungGio = Column(String(20), nullable=False)
+    books_times = relationship('Books', backref='time', lazy=True)
+
+    def __str__(self):
+        return self.khungGio
+
+class Books(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    booked_date = Column(DateTime, default=datetime.now())
+    patient_id = Column(Integer, ForeignKey(Patient.id,  onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    time_id = Column(Integer, ForeignKey(Time.id), nullable=False)
+
+    def __str__(self):
+        return self.id
+
+
 
 if __name__ == "__main__":
     from app import app
     with app.app_context():
         db.create_all()
-        a = Admin(name='Pham Ngoc Son', email='truongson@gmail.com',
-                    password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), type="admin", ngayTao="07/10/2022")
 
-        d = Doctor(name='Truong Dinh Cuong', email='nhatcuong@gmail.com',
-                    password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), type="doctor", ngayVaoLam="14/11/2022")
-
-
-
-        u = Patient(name='Truong Dinh Cuong', email='son@gmail.com',
-                    password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), type="patient", sdt="0123456789", diaChi="Ha Noi", namSinh="1999")
-
-
-        db.session.add(u)
-        db.session.add(a)
-        db.session.add(d)
-        db.session.commit()
+        # a = Admin(name='Pham Ngoc Son', email='truongson@gmail.com',
+        #             password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), type="admin", ngayTao="07/10/2022")
+        #
+        # d = Doctor(name='Truong Dinh Cuong', email='nhatcuong@gmail.com',
+        #             password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), type="doctor", ngayVaoLam="14/11/2022")
+        #
+        # db.session.add(a)
+        # db.session.add(d)
+        # db.session.commit()
